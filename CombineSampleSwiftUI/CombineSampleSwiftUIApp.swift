dfbf0c1c25d88c4b23309bb42bda25dc8f6464bd
Brian Ortiz <mateo972000@gmail.com>
2023-04-01 03:58:40 -0400

    Initial Commit

---
+++ CombineSampleSwiftUI/CombineSampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSampleSwiftUIApp.swift
+//  CombineSampleSwiftUI
+//
+//  Created by Brian Ortiz on 2023-04-01.
+//
+
+import SwiftUI
+
+@main
+struct CombineSampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
